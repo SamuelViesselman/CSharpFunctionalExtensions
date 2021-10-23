779011be38fe6cb273924f63d6ea82a611cbc64b
Sam Viesselman <sviesselman@kahlerautomation.com>
2021-10-23 17:25:40 -0500

    Add overload methods for FirstFailureOrSuccess to take in an IEnumerable and return UnitResults

--- CSharpFunctionalExtensions.Tests/ResultTests/Methods/FirstFailureOrSuccessTests.cs
+++ CSharpFunctionalExtensions.Tests/ResultTests/Methods/FirstFailureOrSuccessTests.cs
@@ -1,21 +1,23 @@
 ﻿using FluentAssertions;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace CSharpFunctionalExtensions.Tests.ResultTests.Methods
 {
-    public class FirstFailureOrSuccessTests
+    public class FirstFailureOrSuccessTests : TestBase
     {
         [Fact]
         public void FirstFailureOrSuccess_returns_the_first_failed_result()
         {
             Result result1 = Result.Success();
-            Result result2 = Result.Failure("Failure 1");
-            Result result3 = Result.Failure("Failure 2");
+            Result result2 = Result.Failure(ErrorMessage);
+            Result result3 = Result.Failure(ErrorMessage + ErrorMessage);
 
             Result result = Result.FirstFailureOrSuccess(result1, result2, result3);
 
             result.IsFailure.Should().BeTrue();
-            result.Error.Should().Be("Failure 1");
+            result.Error.Should().Be(ErrorMessage);
             result.Should().Be(result2);
         }
 
@@ -30,6 +32,97 @@ namespace CSharpFunctionalExtensions.Tests.ResultTests.Methods
 
             result.IsSuccess.Should().BeTrue();
             result.Should().Be(Result.Success());
+        }
+
+        [Fact]
+        public void FirstFailureOrSuccess_ienumerable_returns_the_first_failed_result()
+        {
+            IEnumerable<Result> results = new Result[]
+            {
+                Result.Success(),
+                Result.Failure(ErrorMessage),
+                Result.Failure(ErrorMessage + ErrorMessage),
+            };
+            
+            Result result = Result.FirstFailureOrSuccess(results);
+
+            result.IsFailure.Should().BeTrue();
+            result.Error.Should().Be(ErrorMessage);
+            result.Should().Be(results.ElementAt(1));
+        }
+
+        [Fact]
+        public void FirstFailureOrSuccess_ienumerable_returns_success_if_no_failures()
+        {
+            IEnumerable<Result> results = new Result[]
+            {
+                Result.Success(),
+                Result.Success(),
+                Result.Success(),
+            };
+            
+            Result result = Result.FirstFailureOrSuccess(results);
+
+            result.IsSuccess.Should().BeTrue();
+            result.Should().Be(Result.Success());
+        }
+
+        [Fact]
+        public void FirstFailureOrSuccess_unitresult_returns_the_first_failed_result()
+        {
+            UnitResult<E> result1 = UnitResult.Success<E>();
+            Result<T, E> result2 = Result.Failure<T, E>(E.Value);
+            Result<K, E> result3 = Result.Failure<K, E>(E.Value);
+
+            UnitResult<E> result = Result.FirstFailureOrSuccess(result1, result2, result3);
+
+            result.IsFailure.Should().BeTrue();
+            result.Error.Should().Be(E.Value);
+        }
+
+        [Fact]
+        public void FirstFailureOrSuccess_unitresult_returns_success_if_no_failures()
+        {
+            UnitResult<E> result1 = UnitResult.Success<E>();
+            Result<T, E> result2 = Result.Success<T, E>(T.Value);
+            Result<K, E> result3 = Result.Success<K, E>(K.Value);
+
+            UnitResult<E> result = Result.FirstFailureOrSuccess(result1, result2, result3);
+
+            result.IsSuccess.Should().BeTrue();
+            result.Should().Be(UnitResult.Success<E>());
+        }
+
+        [Fact]
+        public void FirstFailureOrSuccess_ienumerable_unitresult_returns_the_first_failed_result()
+        {
+            IEnumerable<UnitResult<E>> results = new UnitResult<E>[]
+            {
+                UnitResult.Success<E>(),
+                Result.Failure<T, E>(E.Value),
+                Result.Failure<K, E>(E.Value),
+            };
+            
+            UnitResult<E> result = Result.FirstFailureOrSuccess(results);
+
+            result.IsFailure.Should().BeTrue();
+            result.Error.Should().Be(E.Value);
+        }
+
+        [Fact]
+        public void FirstFailureOrSuccess_ienumerable_unitresult_returns_success_if_no_failures()
+        {
+            IEnumerable<UnitResult<E>> results = new UnitResult<E>[]
+            {
+                UnitResult.Success<E>(),
+                Result.Success<T, E>(T.Value),
+                Result.Success<K, E>(K.Value),
+            };
+            
+            UnitResult<E> result = Result.FirstFailureOrSuccess(results);
+
+            result.IsSuccess.Should().BeTrue();
+            result.Should().Be(UnitResult.Success<E>());
         }
     }
 }
--- CSharpFunctionalExtensions/Result/Methods/FirstFailureOrSuccess.cs
+++ CSharpFunctionalExtensions/Result/Methods/FirstFailureOrSuccess.cs
@@ -1,4 +1,6 @@
-﻿namespace CSharpFunctionalExtensions
+﻿using System.Collections.Generic;
+
+namespace CSharpFunctionalExtensions
 {
     public partial struct Result
     {
@@ -7,6 +9,13 @@
         ///     If there is no failure, a success result is returned.
         /// </summary>
         public static Result FirstFailureOrSuccess(params Result[] results)
+            => FirstFailureOrSuccess(results as IEnumerable<Result>);
+
+        /// <summary>
+        ///     Returns the first failure from the supplied <paramref name="results"/>.
+        ///     If there is no failure, a success result is returned.
+        /// </summary>
+        public static Result FirstFailureOrSuccess(IEnumerable<Result> results)
         {
             foreach (Result result in results)
             {
@@ -15,6 +24,28 @@
             }
 
             return Success();
+        }
+
+        /// <summary>
+        ///     Returns the first failure from the supplied <paramref name="results"/>.
+        ///     If there is no failure, a success result is returned.
+        /// </summary>
+        public static UnitResult<E> FirstFailureOrSuccess<E>(params UnitResult<E>[] results)
+            => FirstFailureOrSuccess(results as IEnumerable<UnitResult<E>>);
+
+        /// <summary>
+        ///     Returns the first failure from the supplied <paramref name="results"/>.
+        ///     If there is no failure, a success result is returned.
+        /// </summary>
+        public static UnitResult<E> FirstFailureOrSuccess<E>(IEnumerable<UnitResult<E>> results)
+        {
+            foreach (UnitResult<E> result in results)
+            {
+                if (result.IsFailure)
+                    return result;
+            }
+
+            return UnitResult.Success<E>();
         }
     }
 }
